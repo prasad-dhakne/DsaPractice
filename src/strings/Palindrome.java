f95cabf1d76a2bf3df19675d66a0f35e07bf69bf
prasad2411 <dhakaneprasad@gmail.com>
2022-07-19 15:38:16 +0530

    added string/palindrome

---
+++ src/strings/Palindrome.java
@@ -0,0 +1,23 @@
+package strings;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		String str = "abcdfdcba";
+		System.out.println(isPalindrome(str));
+	}
+
+	private static boolean isPalindrome(String str) {
+		int start = 0;
+		int end = str.length() - 1;
+		while(start <= end) {
+			if(str.charAt(start) == str.charAt(end)) {
+				start+=1;
+				end-=1;
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
+	}
+}
